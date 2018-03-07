55dc6c6322eb3f314855a4298ea3b13d4cf0ea86
JavierIslas <Dimcairion@live.com>
2018-03-07 15:21:15 -0300

    All unused classes deleted

--- BiciProt/Source/BiciProt/BiciProt.Build.cs
+++ BiciProt/Source/BiciProt/BiciProt.Build.cs
@@ -8,8 +8,7 @@ public class BiciProt : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysXVehicles", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysXVehicles" });
 
-		Definitions.Add("HMD_MODULE_INCLUDED=1");
 	}
 }
---
+++ BiciProt/Source/BiciProt/Private/BiciProtGameMode.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "BiciProtGameMode.h"
+
+
+ABiciProtGameMode::ABiciProtGameMode()
+{
+
+}
